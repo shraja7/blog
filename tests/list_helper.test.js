a1367ab19bd022f4c6f8eabcb28d177b7f3dfb74
shraja7 <shraja7@gmail.com>
2023-03-27 20:56:39 -0500

    Add test suite for totalLikes function
    This commit adds a new test suite to the `list_helper.test.js` file that tests the `totalLikes` function. The test suite includes three individual test cases that check the behavior of the function when called with different sets of input data. All test cases pass successfully.

--- tests/list_helper.test.js
+++ tests/list_helper.test.js
@@ -6,3 +6,50 @@ test("dummy returns one", () => {
   const result = listHelper.dummy(blogs);
   expect(result).toBe(1);
 });
+
+//describe block for testing totalLikes
+describe("total likes", () => {
+  test("of empty list is zero", () => {
+    const blogs = [];
+    const result = listHelper.totalLikes(blogs);
+    expect(result).toBe(0);
+  });
+
+  test("when list has only one blog equals the likes of that blog", () => {
+    const blogs = [
+      {
+        title: "Blog post 1",
+        author: "Author 1",
+        url: "http://example.com/1",
+        likes: 10,
+      },
+    ];
+    const result = listHelper.totalLikes(blogs);
+    expect(result).toBe(10);
+  });
+
+  test("of a bigger list is calculated right", () => {
+    const blogs = [
+      {
+        title: "Blog post 1",
+        author: "Author 1",
+        url: "http://example.com/1",
+        likes: 10,
+      },
+      {
+        title: "Blog post 2",
+        author: "Author 2",
+        url: "http://example.com/2",
+        likes: 5,
+      },
+      {
+        title: "Blog post 3",
+        author: "Author 3",
+        url: "http://example.com/3",
+        likes: 3,
+      },
+    ];
+    const result = listHelper.totalLikes(blogs);
+    expect(result).toBe(18);
+  });
+});
