7f4335e838cdcb788bc40597c893619ffa2aac94
shraja7 <shraja7@gmail.com>
2023-04-04 15:41:07 -0500

    implement user model

--- server/models/blog.js
+++ server/models/blog.js
@@ -5,6 +5,11 @@ const blogSchema = new mongoose.Schema({
   author: String,
   url: String,
   likes: Number,
+  //refence to user
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
 });
 
 const Blog = mongoose.model("Blog", blogSchema);
